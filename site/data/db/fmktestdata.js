8a7b57f9be2d092fdd12d82d4da5665b6642e659
Danny Hobart <danny@elephantventures.com>
2014-12-04 18:02:34 -0500

    adding directory structure with prebuilt db

---
+++ site/data/db/fmktestdata.js
@@ -0,0 +1,42 @@
+var mydocuments= [
+
+{User : "User", Icon : "Icon", username : "Username", scores : [1,2,3,4,5], friends : [{user : "User", rating : 0}, {user : "User", rating : 1}] },
+
+{User : "James", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "The Jimmer", scores : [2,2,4,4,1], friends : [{user : "Jane", rating : 0}, {user : "Jack", rating : 2}] },
+
+{User : "Jane", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Janiac", scores : [0,2,1,5,4,4,1], friends : [{user : "Jerimiah", rating : 2}, {user : "Julius", rating : 2}, {user : "Jupiter", rating : 1}] },
+
+{User : "Jack", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Jack Attack", scores : [0,1], friends : [{user : "Judgement", rating : 2}] },
+
+{User : "John", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Johnny", scores : [0,5,3,5], friends : [{user : "Jerimiah", rating : 0}, {user : "Jachyn", rating : 2}, {user : "Jill", rating : 2}, {user : "June", rating : 0}] },
+
+{User : "Justin", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Justine", scores : [0], friends : [{user : "Jameson", rating : 0}] },
+
+{User : "Jerimiah", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "JJ", scores : [5,2,3,1,4,3,5], friends : [{user : "Jane", rating : 2}, {user : "James", rating : 2}, {user : "Jachyn", rating : 1}, {user : "June", rating : 0}, {user : "John", rating : 3}] },
+
+{User : "Jachyn", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Jackie", scores : [5,5], friends : [{user : "Jill", rating : 0}, {user : "Justin", rating : 2}, {user : "Jerimiah", rating : 0}, {user : "Juniper", rating : 0}] },
+
+{User : "Jacklyn", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Lynnn", scores : [5,3,2,3,3], friends : [{user : "John", rating : 0}, {user : "Julius", rating : 1}, {user : "Jessica", rating : 0}] },
+
+{User : "Julius", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Jewels", scores : [5,4,4,1,5,1,5], friends : [{user : "James", rating : 2}, {user : "Jacklyn", rating : 2}, {user : "June", rating : 0}, {user : "Jill", rating : 2}, {user : "Jill", rating : 1}] },
+
+{User : "Jerilyn", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Jeri", scores : [1,1,3,2,2,5,4,1,2], friends : [{user : "Jack", rating : 0}, {user : "Julius", rating : 0}] },
+
+{User : "Jameson", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Son", scores : [1,0,3,4,1,2], friends : [{user : "James", rating : 2}, {user : "Julius", rating : 1}, {user : "Jupiter", rating : 1}] },
+
+{User : "June", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "June", scores : [1,1,2,3, 4, 4], friends : [{user : "Jachyn", rating : 2}, {user : "Juniper", rating : 0}, {user : "Jack", rating : 0}] },
+
+{User : "Juniper", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Purple", scores : [1,0,0,2,2,2], friends : [{user : "Jerimiah", rating : 0}, {user : "Jack", rating : 1}, {user : "Julius", rating : 0}, {user : "Juniper", rating : 1}, {user : "Jill", rating : 1}, {user : "Jupiter", rating : 2}] },
+
+{User : "Judgement", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "The Judge", scores : [4,1,1,2,3], friends : [{user : "Jack", rating : 0}, {user : "Julius", rating : 1}, {user : "Jill", rating : 2}, {user : "Jessica", rating : 0}] },
+
+{User : "Jill", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Ill Jill", scores : [1,2,2,4,4,3,1,1,2], friends : [{user : "June", rating : 0}, {user : "Justin", rating : 1}, {user : "Julius", rating : 2}, {user : "John", rating : 2}] },
+
+{User : "Jessica", Icon : "http://thumbs.media.smithsonianmag.com//filer/b9/d2/b9d271f3-7f66-4132-b5af-7d33844505b7/goat.jpg__800x600_q85_crop.jpg", username : "Jess", scores : [1,2,3,3,4,3,3], friends : [{user : "Jerimiah", rating : 0}, {user : "John", rating : 1}, {user : "Jill", rating : 2}, {user : "Jacklyn", rating : 2}, {user : "Jane", rating : 2}] }
+
+]
+
+db.fmktest.insert( mydocuments );
+
+
+
